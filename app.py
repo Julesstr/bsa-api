acb4b1704d28735b3e1fd6a00172cfefbacb3deb
Julesstr <133604961+Julesstr@users.noreply.github.com>
2025-03-04 10:37:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+webhook_url = os.environ.get("WEBHOOK_URL")
 
 @app.route('/', methods=["POST"])
 def hello_world():
-    return token
+    return webhook_url
